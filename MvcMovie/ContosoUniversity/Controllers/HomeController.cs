e12d3beed7e9b45f0ac8b49fdc0f56bb568b8c46
Mario Andres Botero Florez <mariobot113@hotmail.com>
2017-11-07 16:53:31 -0500

    Sorting, filtering, paging, and grouping - EF Core with ASP.NET Core MVC tutorial (3 of 10)

--- MvcMovie/ContosoUniversity/Controllers/HomeController.cs
+++ MvcMovie/ContosoUniversity/Controllers/HomeController.cs
@@ -5,11 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,9 +26,13 @@ namespace ContosoUniversity.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in _context.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup() {
+                                                       EnrollmentDate = dateGroup.Key, StudentCount = dateGroup.Count()
+                                                   };            
 
-            return View();
+            return View(data.ToList());
         }
 
         public IActionResult Contact()

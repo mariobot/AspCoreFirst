b5bc4271340e10382d0c4a326b04ef81662a3139
Mario Andres Botero Florez <mariobot113@hotmail.com>
2017-11-10 16:31:25 -0500

    Creating Backend Services for Native Mobile Applications

---
+++ MvcMovie/ToDoApi2/Controllers/ToDoItemsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi2.Interfaces;
+using ToDoApi2.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace ToDoApi2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly IToDoRepository _toDoRepository;
+
+        public ToDoItemsController(IToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(_toDoRepository.All);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var item = _toDoRepository.Find(id);
+
+            return Ok(item);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+                }
+                bool itemExists = _toDoRepository.DoesItemExist(item.ID);
+                if (itemExists)
+                {
+                    return StatusCode(StatusCodes.Status409Conflict, ErrorCode.TodoItemIDInUse.ToString());
+                }
+                _toDoRepository.Insert(item);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotCreateItem.ToString());
+            }
+            return Ok(item);
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public IActionResult Edit([FromBody] ToDoItem item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+                }
+                var existingItem = _toDoRepository.Find(item.ID);
+                if (existingItem == null)
+                {
+                    return NotFound(ErrorCode.RecordNotFound.ToString());
+                }
+                _toDoRepository.Update(item);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotUpdateItem.ToString());
+            }
+            return NoContent();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                var item = _toDoRepository.Find(id);
+                if (item == null)
+                {
+                    return NotFound(ErrorCode.RecordNotFound.ToString());
+                }
+                _toDoRepository.Delete(id);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotDeleteItem.ToString());
+            }
+            return NoContent();
+        }
+
+        public enum ErrorCode
+        {
+            TodoItemNameAndNotesRequired,
+            TodoItemIDInUse,
+            RecordNotFound,
+            CouldNotCreateItem,
+            CouldNotUpdateItem,
+            CouldNotDeleteItem
+        }
+    }
+}
